3edc38082194f44c667665f5298b7d902ff8e1a4
zackattack2130 <knoxzackery@gmail.com>
2025-09-16 19:17:09 -0700

    Create rules.mk

---
+++ rules.mk
@@ -0,0 +1,8 @@
+SPLIT_KEYBOARD = yes
+OLED_ENABLE    = yes
+OLED_DRIVER    = ssd1306
+OLED_TRANSPORT = i2c
+SERIAL_DRIVER  = bitbang
+WPM_ENABLE     = yes
+LTO_ENABLE     = yes
+LAYOUTS        = split_3x7
